6331155e658f277935692faeeb0211a9f61fc64a
Maël Baron <mael.baron@laposte.net>
2018-10-21 19:40:54 +0200

    Ajout des fichiers propositions

--- src/AppBundle/Controller/PropositionsController.php
+++ src/AppBundle/Controller/PropositionsController.php
@@ -2,9 +2,33 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Propositions;
+use AppBundle\Form\PropositionsType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class PropositionsController extends Controller
 {
+    /**
+     *
+     * @Route("/add, name="addProposition")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+
+    public function addAction()
+    {
+        //On crée une nouvelle proposition
+        $proposition = new Propositions();
+
+        //On récupère le form
+        $form = $this->createForm(PropositionsType::class, $proposition);
+
+        //On génére le fichier final
+        $formView = $form->createView();
+
+        //on rend la vue
+        return $this->render('propositionAdd.html.twig', array('form'=>$formView));
+
+    }
 }
