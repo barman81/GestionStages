fcc7fde78f9dcac31a8247d6046fa917c85d6fe1
Maël Baron <mael.baron@laposte.net>
2018-10-25 17:03:33 +0200

    Modification controller (DomaineactiviteController.php) : submit possible et insertion dans la bd ok !
    Adresse pour voir le form :
    domaineactivite/add

--- src/AppBundle/Controller/DomaineactiviteController.php
+++ src/AppBundle/Controller/DomaineactiviteController.php
@@ -6,6 +6,8 @@ use AppBundle\Entity\Domaineactivite;
 use AppBundle\Form\DomaineactiviteType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DomaineactiviteController extends Controller
 
@@ -18,13 +20,30 @@ class DomaineactiviteController extends Controller
      *
      */
 
-    public function addAction()
+    public function addAction(Request $request)
     {
         //On crée un nouveau domaine d'activité
         $domaineAct = new Domaineactivite();
 
         //On récupère le form
         $form = $this->createForm(DomaineactiviteType::class, $domaineAct);
+
+        $form->handleRequest($request);
+
+        //si le formulaire a été soumis
+
+        if($form->isSubmitted()){
+
+            //on enregistre le domaine d'activité dans la bdd
+            $em = $this-> getDoctrine()->getManager();
+            $em->persist($domaineAct);
+            $em->flush();
+
+            //A la place d'une reponse, il faut renvoyer une vue ! (style avec la liste des domaines...)
+            return new Response('Domaine Ajouté !');
+
+        }
+
 
         //On génére le fichier final
         $formView = $form->createView();
