dc6fe73a67476ece305d9838ea63ed75a2f0d612
Maël Baron <mael.baron@laposte.net>
2018-10-25 18:34:54 +0200

    Modification pour intégrer le style de base Edition d'un domaine de la bdd fonctionnel (utilisation du meme formulaire que pour l'ajout)

--- src/AppBundle/Controller/DomaineactiviteController.php
+++ src/AppBundle/Controller/DomaineactiviteController.php
@@ -49,7 +49,7 @@ class DomaineactiviteController extends Controller
         $formView = $form->createView();
 
         //on rend la vue
-        return $this->render('domainesActivitesShow.html.twig', array('form'=>$formView));
+        return $this->render('domainesActivitesAdd.html.twig', array('form'=>$formView));
 
     }
 
