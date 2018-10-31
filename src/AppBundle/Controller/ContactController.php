75ac7b771b2d56a550a6c8071f8fdce853657a1c
AxelCom* <axel.commergnat@hotmail.com>
2018-10-31 14:06:02 +0100

    Suppresion use inutile et rename modal de supprssion contact

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -17,7 +17,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\Validator\Tests\Fixtures\ToString;
 
 class ContactController extends Controller
 
