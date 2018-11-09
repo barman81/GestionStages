dadadf6b541a535852b6c43c352dfe6b6d1a609c
Mathieu <mathieu@math98.fr>
2018-11-09 11:53:36 +0100

    Met en place les tests
    - Ajoute le phpUnit de Symfony au composer.json
    - Ajoute le .phar de phpUnit (Pour utiliser l'intégration dans l'IDE)
    - Ajoute la classe de test du controlleur Propositions

---
+++ tests/PropositionsControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mathieu
+ * Date: 09/11/18
+ * Time: 11:31
+ */
+
+namespace Tests;
+
+use AppBundle\Controller\PropositionsController;
+use PHPUnit\Framework\TestCase;
+
+class PropositionsControllerTest extends TestCase
+{
+
+    public function testAddAction()
+    {
+
+    }
+
+    public function testShowProposition()
+    {
+
+    }
+
+    public function testShowPropositionById()
+    {
+
+    }
+}
